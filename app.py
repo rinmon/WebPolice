7e462000b518ba0f59622ae3479c5eb951d6560c
Rinmon <rinmon@cn7.jp>
2025-05-25 12:23:01 +0900

    feat: add ProxyFix middleware to handle X-Forwarded headers in Flask app

--- app.py
+++ app.py
@@ -6,12 +6,14 @@ import datetime # For formatting date from Wayback Machine
 from bs4 import BeautifulSoup
 from weasyprint import HTML
 from io import BytesIO
+from werkzeug.middleware.proxy_fix import ProxyFix
 import dns.resolver
 import socket # For gethostbyname
 
 APP_VERSION = "0.9.0"
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_script_name=1)
 app.config['APPLICATION_ROOT'] = '/tools/webpolice'
 
 # Helper function to get DNS information
